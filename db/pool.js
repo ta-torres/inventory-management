5bea3c6fbd6ac78c96cf6255689d9fde8b712352
Thomas <93293510+ta-torres@users.noreply.github.com>
2025-06-06 11:42:27 -0300

    test

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
